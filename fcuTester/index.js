3ef6648d04abb5b04d654333f2944440ddd102c0
unknown <gbox3d@gmail.com>
2019-11-29 00:03:45 +0900

    fcutester serialport 802 update

--- fcuTester/index.js
+++ fcuTester/index.js
@@ -17,16 +17,31 @@ var theApp = {
   }
 }
 
-serialport.list(function (err, ports) {
-  ports.forEach(function(port) {
-    let _li = document.createElement('li');
-    _li.innerText = port.comName + "(" + port.manufacturer + ")";
-    _li.comName = port.comName;
 
-    document.querySelector('#portList').appendChild(_li);
+serialport.list().then(
+  ports => {
+    ports.forEach(port=> {
+      let _li = document.createElement('li');
+      _li.innerText = port.comName + "(" + port.manufacturer + ")";
+      _li.comName = port.comName;
 
-  });
-});
+      document.querySelector('#portList').appendChild(_li);
+
+    })
+  },
+  err => console.error(err)
+)
+
+// serialport.list(function (err, ports) {
+//   ports.forEach(function(port) {
+//     let _li = document.createElement('li');
+//     _li.innerText = port.comName + "(" + port.manufacturer + ")";
+//     _li.comName = port.comName;
+
+//     document.querySelector('#portList').appendChild(_li);
+
+//   });
+// });
 
 document.querySelector("#portList").addEventListener('click',function (evt) {
 
